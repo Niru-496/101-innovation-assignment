c83991065308879595cbe7ee06dc7216cc8083d3
Niranjan <narayana9492324464@gmail.com>
2022-07-01 18:35:22 +0530

    tried to add items to favorites

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { useNavigate } from "react-router";
 
 
 export default function Header() {
-
+	const navigate = useNavigate()
 	return (
-		<div className="head-container">
+		<div
+			className="head-container"
+			onClick={() => navigate("/")}
+		>
 			<h2 className="head-title">TheFoodList</h2>
-
 		</div>
 	);
 };
--- src/components/SingleProductDispalyer.jsx
+++ src/components/SingleProductDispalyer.jsx
@@ -3,43 +3,45 @@ import React, { useEffect, useState } from "react";
 import icon from "../images/Icon.png";
 import { useParams } from "react-router";
 import Header from "./Header";
-
+import Button from "@mui/material/Button";
 export default function SingleProduct() {
 	const { code } = useParams();
 
-	const [singledata, setSingledata] = useState({});
+	const [product, SetProduct] = useState({});
 
 	useEffect(() => {
 		axios
 			.get(
 				`https://innovative101assignement.herokuapp.com/foodData?code=${code}`
 			)
-			.then((res) => setSingledata(res.data[0]));
+			.then((res) => SetProduct(res.data[0]));
 	}, [code]);
-
 
 	return (
 		<div>
 			<Header />
 
 			<div className="details">
-				<div className="prod_name">
-					<img src={icon} alt="not found" className="iconimgg" />
-					<h3 className="prodalign">{singledata.product_name}</h3>
+				<div className="Product-container-div">
+					<img src={icon} alt="not found" className="Icon-img-src" />
+					<h4 className="Product-name-tag">{product.product_name}</h4>
+					<Button variant="contained" onClick={()=>{ }}> + Favourities</Button>
 				</div>
 
-				<table>
-					<tbody>
-						{Object.keys(singledata).map((key, index) => {
-							return (
-								<tr key={index}>
-									<td>{key}</td>
-									<td>{singledata[key]}</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
+				<div className="table-main-container">
+					<table>
+						<tbody>
+							{Object.keys(product).map((key, index) => {
+								return (
+									<tr key={index}>
+										<td>{key}</td>
+										<td>{product[key]}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	);
